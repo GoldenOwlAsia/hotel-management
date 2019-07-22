769edbd1c2e80df8c116279345082d4bb08b3c4d
Thanh Truong <thanhtruong1216@gmail.com>
2019-07-22 11:22:53 +0700

    chore: create model role

---
+++ db/migrate/20190720130455_create_roles.rb
@@ -0,0 +1,11 @@
+class CreateRoles < ActiveRecord::Migration[6.0]
+  def change
+    create_table :roles do |t|
+      t.string :name
+      t.references :user, null: false, foreign_key: true
+      t.references :hotel, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
