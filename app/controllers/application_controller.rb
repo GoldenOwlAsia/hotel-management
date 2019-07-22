154b3c990a180d039c59823eb895ae5b94ca7049
Thanh Truong <thanhtruong1216@gmail.com>
2019-07-22 20:24:45 +0700

    fix: quick filter rooms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   include Pundit
   protect_from_forgery
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || begin
+      hotels = current_user.hotels
+      hotels.present? ? hotel_rooms_path(hotels.first) : root_path
+    end
+  end
 end
--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,15 +3,21 @@ class RoomsController < ApplicationController
     @hotel = Hotel.find(params[:hotel_id])
     @hotel_id = @hotel.id
     @hotel_name = @hotel.name
+
     @rooms = Room.where(hotel_id: params[:hotel_id])
     @rooms = @rooms.where(room_type: room_type_param) if room_type_param
     @rooms = @rooms.where('room_number ILIKE ?', "%#{search_param}%") if search_param
     @standard_rooms = @rooms.standard
     @superior_rooms = @rooms.superior
     @vip_rooms = @rooms.vip
-    @standard_rooms_available = @standard_rooms.where(status: 'available')
-    @superior_rooms_available = @superior_rooms.where(status: 'available')
-    @vip_rooms_available = @vip_rooms.where(status: 'available')
+
+    @available_rooms = Room.where(hotel_id: params[:hotel_id], status: 'available')
+                           .group(:room_type)
+                           .count
+  end
+
+  def show
+    @room = Room.find(params[:id])
   end
 
   private
