8f7c5dac09a5eb81b2495a724725b67398d5e697
Thanh Truong <thanhtruong1216@gmail.com>
2019-07-22 17:49:11 +0700

    feat: remove room status text

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -1,14 +1,4 @@
 module RoomsHelper
-  def get_rom_status_text(room)
-    if room.status == 'booked'
-      'Đã đặt phòng'
-    elsif room.status == 'rent'
-      'Phòng đang thuê'
-    else
-      'Phòng trống'
-    end
-  end
-
   def get_rom_status_class(room)
     "room-status-#{room.status}"
   end
