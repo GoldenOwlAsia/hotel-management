47b281761f55bd6ff90f307a418952015c6468c1
Thanh Truong <thanhtruong1216@gmail.com>
2019-07-24 23:37:12 +0700

    fix: nested resources

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,7 @@ Rails.application.routes.draw do
   root 'homes#index'
   devise_for :users, controllers: { registrations: 'registrations' }
   resources :hotels do
-    resources :rooms, only: [:index]
+    resources :rooms
   end
-
-  resources :rooms do
-    member do
-      post :booking
-    end
-  end
+  resources :bookings
 end
