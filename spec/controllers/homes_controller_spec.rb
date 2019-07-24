67a5f4de4900781731211f7c8f1df57a80b59f45
Thanh Truong <thanhtruong1216@gmail.com>
2019-07-24 17:06:23 +0700

    style: filter box

--- spec/controllers/homes_controller_spec.rb
+++ spec/controllers/homes_controller_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe HomesController, type: :controller do
 
   describe "GET #index" do
+    let(:user) { create(:user) }
     it "returns http success" do
+      sign_in user
       get :index
       expect(response).to have_http_status(:success)
     end
