572b3eb63b5b6673a1068fa052463f55fcfb7a07
Thanh Truong <thanhtruong1216@gmail.com>
2019-07-25 21:19:12 +0700

    test: use shoulda-matchers

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :user do
-    email { 'hongvan@gmail.com'}
-    user_name { 'Pika' }
-    first_name { 'Van' }
-    last_name { 'Ngo' }
-    password {'opensesame'}
+    email { Faker::Internet.email }
+    user_name { Faker::Name.name }
+    first_name { Faker::Name.name }
+    last_name { Faker::Name.name }
+    password { 'opensesame' }
   end
 end
