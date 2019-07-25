e467c8b3a3af2c30db5b51bfee1a5594ff6d982e
Thanh Truong <thanhtruong1216@gmail.com>
2019-07-25 08:17:15 +0700

    fix: rooms resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'booking/new'
   root 'homes#index'
   devise_for :users, controllers: { registrations: 'registrations' }
   resources :hotels do
     resources :rooms
   end
   resources :bookings
+  resources :rooms
 end
