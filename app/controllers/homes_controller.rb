54557d7158a9bd878451388ce324489a875879d6
Thanh Truong <thanhtruong1216@gmail.com>
2019-07-29 13:05:42 +0700

    fix: use rooms_path instead of rooms_url

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,7 @@
 class HomesController < ApplicationController
   def index
     if current_user.hotels.first.present?
-      redirect_to rooms_url(hotel_id: current_user.hotels.first.id)
+      redirect_to rooms_path(hotel_id: current_user.hotels.first.id)
     else
       @hotels = current_user.hotels
     end
