e53636b667d7546fd5dbdb97d3c471b5658325b2
Thanh Truong <thanhtruong1216@gmail.com>
2019-07-31 23:08:06 +0700

    feat: add guests to room

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -28,6 +28,11 @@ class BookingsController < ApplicationController
     )
 
     if @booking.save!
+      @booking.guests.create(guest_type: 'women', quantity: women_count) if women_count > 0
+      @booking.guests.create(guest_type: 'men', quantity: men_count) if men_count > 0
+      @booking.guests.create(guest_type: 'baby_girl', quantity: baby_girl_count) if baby_girl_count > 0
+      @booking.guests.create(guest_type: 'baby_boy', quantity: baby_boy_count) if baby_boy_count > 0
+
       redirect_to rooms_path(hotel_id: Room.find(params[:room_id]).hotel.id)
     else
       render 'new'
@@ -41,7 +46,38 @@ class BookingsController < ApplicationController
     @booking = Booking.find(params[:id])
   end
 
+  def women_count
+    @women_count ||= booking_params[:women].to_i
+  end
+
+  def men_count
+    @men_count ||= booking_params[:men].to_i
+  end
+
+  def baby_girl_count
+    @baby_girl_count ||= booking_params[:baby_girl].to_i
+  end
+
+  def baby_boy_count
+    @baby_boy_count ||= booking_params[:baby_boy].to_i
+  end
+
   def booking_params
-    params.require(:room_bookings).permit(:checkin_time, :checkout_time, :booked_at, :room_id, :customer_id, :status, :name, :phone_number, :customer_nin, :rent_type)
+    params.require(:room_bookings).permit(
+      :checkin_time,
+      :checkout_time,
+      :booked_at,
+      :room_id,
+      :customer_id,
+      :status,
+      :name,
+      :phone_number,
+      :customer_nin,
+      :rent_type,
+      :women,
+      :men,
+      :baby_girl,
+      :baby_boy
+    )
   end
 end
--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,7 +2,7 @@ class HomesController < ApplicationController
   def index
     first_hotel = current_user.hotels.first
     if first_hotel.present?
-      redirect_to rooms_path(hotel_id: first_hotel.id)
+      redirect_to hotel_rooms_path(first_hotel)
     else
       @hotels = current_user.hotels
     end
