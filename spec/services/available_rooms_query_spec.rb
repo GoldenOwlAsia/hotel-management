1fc77207491804f5ed0f9f0adce8ee5d17b96c35
Thanh Truong <thanhtruong1216@gmail.com>
2019-08-01 12:27:59 +0700

    test: add test for models

--- spec/services/available_rooms_query_spec.rb
+++ spec/services/available_rooms_query_spec.rb
@@ -4,25 +4,25 @@ describe AvailableRoomsQuery do
   let!(:room_1) { create(:room, hotel: hotel) }
   let!(:room_2) { create(:room, hotel: hotel) }
 
-  # context 'no rooms booked' do
-  #   it 'returns available rooms' do
-  #     expect(service.execute(Time.current, 1.day.from_now)).to eq([room_1, room_2])
-  #   end
-  # end
+  context 'no rooms booked' do
+    it 'returns available rooms' do
+      expect(service.execute(Time.current, 1.day.from_now)).to eq([room_1, room_2])
+    end
+  end
 
-  # context 'a room booked' do
-  #   let!(:booking) { create(:booking, room: room_1, checkin_time: 2.day.ago, checkout_time: 1.days.ago) }
-  #   it 'returns available rooms' do
-  #     expect(service.execute(Time.current, 1.day.from_now)).to eq([room_1, room_2])
-  #   end
-  # end
+  context 'a room booked' do
+    let!(:booking) { create(:booking, room: room_1, checkin_time: 2.day.ago, checkout_time: 1.days.ago) }
+    it 'returns available rooms' do
+      expect(service.execute(Time.current, 1.day.from_now)).to eq([room_1, room_2])
+    end
+  end
 
-  # context 'a room booked' do
-  #   let!(:booking) { create(:booking, room: room_1, checkin_time: 2.day.from_now, checkout_time: 3.day.from_now) }
-  #   it 'returns available rooms' do
-  #     expect(service.execute(Time.current, 1.day.from_now)).to eq([room_1, room_2])
-  #   end
-  # end
+  context 'a room booked' do
+    let!(:booking) { create(:booking, room: room_1, checkin_time: 2.day.from_now, checkout_time: 3.day.from_now) }
+    it 'returns available rooms' do
+      expect(service.execute(Time.current, 1.day.from_now)).to eq([room_1, room_2])
+    end
+  end
 
   context 'a room booked' do
     let!(:booking) { create(:booking, room: room_1, checkin_time: 1.day.from_now, checkout_time: 2.day.from_now) }
