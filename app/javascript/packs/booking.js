f1bb754f4e49fe95894dc46744b5083f6d9523c0
Thanh Truong <thanhtruong1216@gmail.com>
2019-08-02 14:16:18 +0700

    feat: move js code to javascript folder

---
+++ app/javascript/packs/booking.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  $(document).on('click', '.booking__add-customers', function () {
+    $('.booking__customer-icons-wrapper').toggle('show-room-members');
+    console.log('Hello');
+  });
+
+  $(document).on('click', '.plus-btn', function () {
+    let $input = $(this).siblings('.input').find('input');
+    $input.val(Number($input.val()) + 1);
+  });
+
+  $(document).on('click', '.minus-btn', function () {
+    let $input = $(this).siblings('.input').find('input');
+    $input.val(Math.max(Number($input.val()) - 1, 0));
+  });
+
+  $(document).on('click', '.booking__cancel-btn', function () {
+    $('.right-sidebar').hide();
+    $('.overlay').hide();
+  });
+});
