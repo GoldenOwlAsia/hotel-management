439a2fc4defcf0c24a14555abfb76bf21e42017b
Thanh Truong <thanhtruong1216@gmail.com>
2019-08-02 14:16:18 +0700

    feat: move js code to javascript folder

---
+++ app/javascript/packs/header.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+  $(document).on('click', '.sub-header__filter-default', function () {
+    $('.sub-header__filter-content').toggle('hide-filter-content');
+  });
+});
