dc0b295e16ee32b6041a0383211268d80f471333
Thanh Truong <thanhtruong1216@gmail.com>
2019-08-02 13:09:04 +0700

    feat: enum payment method

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
   has_many :service_orders
   has_many :services, through: :service_orders, source: :services
+
+  enum payment_method: { cash: 'cash', credit_card: 'credit_card', bank_transfer: 'bank_transfer' }
 end
